57eb8ba0856c7ddf95413536624dc2094b1ae198
Mariia <mariadenkovic4@gmail.com>
2026-02-02 16:55:31 +0200

    new 3

--- src/db/connectMongoDB.js
+++ src/db/connectMongoDB.js
@@ -1,21 +1,13 @@
+// src/db/connectMongoDB.js
 import mongoose from 'mongoose';
 
 export const connectMongoDB = async () => {
-  const mongoUrl = process.env.MONGO_URL;
-
-  if (!mongoUrl) {
-    console.error('❌ MONGO_URL is not defined in .env');
-    process.exit(1);
-  }
-
   try {
-    await mongoose.connect(mongoUrl, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    const mongoUrl = process.env.MONGO_URL;
+    await mongoose.connect(mongoUrl);
     console.log('✅ MongoDB connection established successfully');
   } catch (error) {
     console.error('❌ Failed to connect to MongoDB:', error.message);
-    process.exit(1);
+    process.exit(1); // аварійне завершення програми
   }
 };
